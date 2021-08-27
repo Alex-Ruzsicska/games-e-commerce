d65d51117fd4bdf0e41a3f6a4084447d69a0a85c
Alex Ruzsicska <alexhruzsi@gmail.com>
2021-08-27 13:28:49 -0300

    Creating folders structure

---
+++ src/screens/Cart/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from './styles';
+
+
+const Cart = () => {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Cart Screen!
+      </Text>
+    </View>
+  );
+}
+
+export default Cart;
---
+++ src/screens/MyAccount/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from './styles';
+
+
+const MyAccount = () => {
+  return (
+    <View style={styles.container}>
+      <Text>
+        MyAccount Screen!
+      </Text>
+    </View>
+  );
+}
+
+export default MyAccount;
