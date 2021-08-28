b691816ce002eba0183f071b81529d51db76b4da
Alex Ruzsicska <alexhruzsi@gmail.com>
2021-08-28 14:34:27 -0300

    Installing react-navigation

--- App.js
+++ App.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import Home from './src/screens/Home';
+import HomeScreen from './src/screens/Home';
+import CartScreen from './src/screens/Cart';
+import MyAccountScreen from './src/screens/MyAccount';
+
+
+import { Provider as PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
+const Tab = createMaterialBottomTabNavigator();
+
 
 export default function App() {
   return (
-    <Home></Home>
+    <PaperProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName="Home"
+          barStyle={{ alignItems: "center", justifyContent: "center"}}
+        >
+          <Tab.Screen name="Home" component={HomeScreen}/>
+          <Tab.Screen name="Carrinho" component={CartScreen} />
+          <Tab.Screen name="Minha Conta" component={MyAccountScreen} />
+
+        </Tab.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
